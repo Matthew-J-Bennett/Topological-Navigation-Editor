5eb1d9d5534e5f0e5b8dba6ecae5b7fb94b7f8a7
Freddie <freddie561@btinternet.com>
2020-02-04 15:05:43 +0000

    GUI Colour and Button Changes
    Basic GUI changes to background and button sizing. Also added a message box popup to test functionality of the open file button

--- elements.py
+++ elements.py
@@ -11,21 +11,22 @@ class Element:
 
 
 class Frame(Element):
-    def __init__(self, master, bg='white', height=800, width=1250):
+    def __init__(self, master, bg='grey24', height=800, width=1250):
         self.element = tk.Frame(master=master, height=height, width=width, bg=bg)
         self.element.pack()
         super()
 
 
 class Button(Element):
-    def __init__(self, master, text, x, y, sequence='<Button-1>', width=10, func=None, font=None,
-                 bg='LightSkyBlue1',
+    def __init__(self, master, text, x, y, sequence='<Button-1>', width=10, height=2, func=None, font=None,
+                 bg='Gray90',
                  bd=4,
                  relief='raised',
                  anchor=None, animation=True):
         self.animation = animation
         self.master = master
-        self.element = tk.Label(master=master, text=text, font=font, bg=bg, bd=bd, relief=relief, width=width)
+        self.element = tk.Label(master=master, text=text, font=font, bg=bg, bd=bd, relief=relief, width=width,
+                                height=height)
         self.element.bind(sequence=sequence, func=lambda x: self.clicked(execute=func))
         super().__init__(self.element, x=x, y=y, anchor=anchor)
 
--- frames/LaunchFrame.py
+++ frames/LaunchFrame.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 import elements
 import frames
@@ -9,15 +9,18 @@ class LaunchFrame:
         self.master = master
         self.logging = master.logger
         self.window = master.master
-        self.frame = elements.Frame(master=self.window)
+        self.frame = elements.Frame(master=self.window, bg="grey24")
 
         swap_frame_tmp = elements.Button(master=master.master, x=10, y=10, text="Swap Frame to Main Project Frame",
                                          width=30, func=lambda: self.master.frame_swap(old_frame=self.frame,
                                                                                        new_frame=lambda: frames.MainFrame(
                                                                                            master=self.master)))
 
-        import_button = elements.Button(master=master.master, x=10, y=40, text="Import Files", width=20,
+        import_button = elements.Button(master=master.master, x=800, y=600, text="Import Files", width=20,
                                         func=lambda: self.getimportfilename())
+
+        open_button = elements.Button(master=master.master, x=1000, y=600, text="Open Files", width=20,
+                                      func=lambda: messagebox.showinfo("Title", "a box"))
 
         if not self.master.launched:
             self.logging.info("Creating Launch Frame")
