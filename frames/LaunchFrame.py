17374822a6cc33fdc7c59dfb3d5a246ea0eac6b5
sam6453 <43754939+sam6453@users.noreply.github.com>
2020-02-05 14:57:28 +0000

    stores file names, parses the yaml file

--- frames/LaunchFrame.py
+++ frames/LaunchFrame.py
@@ -1,8 +1,9 @@
 from tkinter import filedialog
-
+from pathlib import Path
 import elements
 import frames
-
+import yaml
+import PIL
 
 class LaunchFrame:
     def __init__(self, master):
@@ -10,15 +11,12 @@ class LaunchFrame:
         self.logging = master.logger
         self.window = master.master
         self.frame = elements.Frame(master=self.window)
-
         swap_frame_tmp = elements.Button(master=master.master, x=10, y=10, text="Swap Frame to Main Project Frame",
                                          width=30, func=lambda: self.master.frame_swap(old_frame=self.frame,
                                                                                        new_frame=lambda: frames.MainFrame(
-                                                                                           master=self.master)))
-
+                                                                                            master=self.master)))
         import_button = elements.Button(master=master.master, x=10, y=40, text="Import Files", width=20,
                                         func=lambda: self.getimportfilename())
-
         temp_save_button = elements.Button(master=master.master, x=10, y=90, text="Save Files", width=20)
 
         if not self.master.launched:
@@ -26,5 +24,42 @@ class LaunchFrame:
             self.window.mainloop()
 
     def getimportfilename(self):
-        self.master.filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
-                                                            filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+        self.master.filenames = filedialog.askopenfilenames(initialdir="/", title="Select file",
+                                                            filetypes=(("Map files", ".yaml .pgm .tmap"), ("all files", "*.*")))
+
+        if self.setfilenames() == 1:
+            self.readyaml()
+            self.readpgm()
+
+    def setfilenames(self):
+        self.master.files = ["","",""]
+        numfiles = 0
+        for x in self.master.filenames:
+            if Path(x).suffix == ".pgm" and self.master.files[0] == "":
+                self.master.files[0] = x
+                print("PGM file imported: ",x,"\n")
+                numfiles += 1
+            elif Path(x).suffix == ".tmap" and self.master.files[1] == "":
+                self.master.files[1] = x
+                print("TMAP file imported: ",x,"\n")
+                numfiles += 1
+            elif Path(x).suffix == ".yaml" and self.master.files[2] == "":
+                self.master.files[2] = x
+                print("YAML file imported: ",x,"\n")
+                numfiles += 1
+            else:
+                print("Invalid file type selected: ", x, "\n")
+        if numfiles != 3:
+            print("Not all files selected. Please select again.")
+            return 0
+        else:
+            return 1
+
+    def readyaml(self):
+        with open(self.master.files[2]) as file:
+            self.master.yamldata = yaml.load(file, Loader=yaml.FullLoader)
+            print(self.master.yamldata)
+            print(self.master.yamldata.get("resolution"))
+
+    def readpgm(self):
+        image = PIL.Image.open(self.master.files[0])
