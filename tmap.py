216eebcb985295b767f828af738ef62bfdb80fce
sam6453 <43754939+sam6453@users.noreply.github.com>
2020-02-11 16:13:01 +0000

    new tmap file

---
+++ tmap.py
@@ -0,0 +1,25 @@
+class Tmap():
+    def __init__(self,map,pointset):
+        self.map = map
+        self.pointset = pointset
+        self.nodelist = []
+        self.nodes = []
+        self.nodetotal = 0
+
+    def addNode(self, nodename, node):
+        self.nodelist.append(nodename)
+        self.nodes.append(node)
+        self.nodetotal = len(self.nodelist)
+
+
+class Node(Tmap):
+    def __init__(self,map,name,pointset,edges,pose,xy,yaw):
+        self.map = map
+        self.name = name
+        self.pointset = pointset
+        self.edges = edges
+        self.pose = pose
+        self.verts = []
+        self.xy_goal_tolerance = xy
+        self.yaw_goal_tolerance = yaw
+        super()
